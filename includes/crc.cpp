18bf9a9fa62fb4aae424be7f2bca61ed8a040a52
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-10-28 22:15:50 +0800

    start code, sleepy na

---
+++ includes/crc.cpp
@@ -0,0 +1,8 @@
+//File Name: crc.cpp
+//Author: George Gesite
+//Activity 5 CPE 411 Embedded System
+//Started 28/10/22
+//Description: CRC C++ Implementation Program 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
---
+++ includes/crc.h
@@ -0,0 +1,14 @@
+//File Name: crc.h
+//Author: George Gesite
+//Activity 5 CPE 411 Embedded System
+//Started 28/10/22
+//Description: CRC C++ Implementation Program 
+#ifndef CHECKSUM_H
+#define CHECKSUM_H
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//enter prototypes
+
+#endif
